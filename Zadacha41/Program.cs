1354b8c4612ba2954d262249149b88eaf8d99301
RusanovSasha <107805109+RusanovSasha@users.noreply.github.com>
2022-10-24 19:17:06 +0300

    Coding

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+int[] EnterNumber(int[] count)
+{   
+    if (count[0] == 0) Console.WriteLine("Начинаем нашу программу для счета нулей. Введите любое количесто чисел подряд.");
+    else Console.WriteLine($"Вы ввели {count[0]} чисел, из них было введено {count[1]} нулей");
+    Console.Write("Введите число (для выхода введите q) ");
+    string enterString = Console.ReadLine();
+    if (enterString == "q" || enterString == "Q") return count;
+    int enterNum;
+    if (int.TryParse(enterString, out enterNum))
+    {
+        count[0]++;
+        if (enterNum == 0) count[1]++;
+    }
+    else Console.WriteLine("Боюсь это было не число. Оно не засчитывается в общей массе");
+    Console.WriteLine();
+    EnterNumber(count);
+    return count;
+}
+
+Console.Clear();
+int[] countNull = { 0, 0 };
+countNull = EnterNumber(countNull);
+Console.Clear();
+Console.WriteLine($"Программа завершена пользователем");
+Console.WriteLine($"Всего было введено {countNull[0]} чисел, из них программа насчитала {countNull[1]} нулей");
---
+++ Zadacha43/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void EnterLines(double[,] lineToCount)
+{
+    if (lineToCount[0, 0] == lineToCount[0, 1]) Console.WriteLine("Линии параллельны");
+    else
+    {
+        double x = (lineToCount[1, 1] - lineToCount[1, 0]) / (lineToCount[0, 0] - lineToCount[0, 1]);
+        double y = lineToCount[0, 0] * x + lineToCount[1, 0];
+        Console.WriteLine($"Координаты пересечения ({x}; {y})");
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Начинаем программу для нахождения точек пересечения двух прямых.");
+
+double[,] lines = new double[2, 2];
+Console.Write("Введите b1 ");
+lines[1, 0] = double.Parse(Console.ReadLine());
+Console.Write("Введите k1 ");
+lines[0, 0] = double.Parse(Console.ReadLine());
+
+Console.Write("Введите b2 ");
+lines[1, 1] = double.Parse(Console.ReadLine());
+Console.Write("Введите k2 ");
+lines[0, 1] = double.Parse(Console.ReadLine());
+
+EnterLines(lines);
